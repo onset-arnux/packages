018153eca886aba8e36408303323ad39022c9160
ArnyDaHamsta <ArnyDaHamsta@gmail.com>
2020-01-21 20:38:04 +0000

    Update watertest.lua

--- watertest/client/watertest.lua
+++ watertest/client/watertest.lua
@@ -4,28 +4,28 @@ function waterlevelmin200()
     SetOceanWaterLevel(-200, true)
     AddPlayerChat("CLIENT RECIEVED")
 end
-AddRemoteEvent("waterlevelmin200", waterlevelmin200)
+AddRemoteEvent("200", waterlevelmin200)
 
 function waterlevelmin1000()
     SetOceanWaterLevel(1000, true)
     AddPlayerChat("CLIENT RECIEVED")
 end
-AddRemoteEvent("waterlevelmin1000", waterlevelmin1000)
+AddRemoteEvent("1000", waterlevelmin1000)
 
 function waterlevelmin5000()
     SetOceanWaterLevel(5000, true)
     AddPlayerChat("CLIENT RECIEVED")
 end
-AddRemoteEvent("waterlevelmin5000", waterlevelmin5000)
+AddRemoteEvent("5000", waterlevelmin5000)
 
 function waterlevelmin10000()
     SetOceanWaterLevel(10000, true)
     AddPlayerChat("CLIENT RECIEVED")
 end
-AddRemoteEvent("waterlevelmin10000", waterlevelmin10000)
+AddRemoteEvent("10000", waterlevelmin10000)
 
 function waterlevelmin0()
     SetOceanWaterLevel(0, true)
     AddPlayerChat("CLIENT RECIEVED")
 end
-AddRemoteEvent("waterlevelmin0", waterlevelmin0)
+AddRemoteEvent("0", waterlevelmin0)
