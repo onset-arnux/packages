16c4b7ae3e1384458e98a5f578cf7fe7ea261000
ArnyDaHamsta <ArnyDaHamsta@gmail.com>
2020-01-21 20:03:35 +0000

    water test

---
+++ watertest/client/watertest.lua
@@ -0,0 +1,29 @@
+function waterlevelmin200(playerid)
+    SetOceanWaterLevel(-200, true)
+	return
+end
+AddCommand("waterlevelmin200", waterlevelmin200)
+
+function waterlevelmin1000(playerid)
+    SetOceanWaterLevel(1000, true)
+	return
+end
+AddCommand("waterlevelmin1000", waterlevelmin1000)
+
+function waterlevelmin5000(playerid)
+    SetOceanWaterLevel(5000, true)
+	return
+end
+AddCommand("waterlevelmin5000", waterlevelmin5000)
+
+function waterlevelmin10000(playerid)
+    SetOceanWaterLevel(10000, true)
+	return
+end
+AddCommand("waterlevelmin10000", waterlevelmin10000)
+
+function waterlevelmin0(playerid)
+    SetOceanWaterLevel(0, true)
+	return
+end
+AddCommand("waterlevelmin0", waterlevelmin0)
